4d2d5c45c6695bec08839cc4890f9573b7dcf8ee
AJ Auntor <auntorknocks@gmail.com>
2021-06-22 11:38:55 +0600

    url added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const searchSongs = () => {
     const searchText = document.getElementById("search-field").value;
-    // const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => console.log(data))
