0f5aeaa618bb021533fd60ae61ddb7fdcc606417
AJ Auntor <auntorknocks@gmail.com>
2021-06-22 11:40:25 +0600

    js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const searchSongs = () => {
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
     .then(res => res.json())
-    
+    .then(data => console.log(data))
 }
