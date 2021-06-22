40fdbe0d7f1148deb3d4d2febb420b88006b958b
AJ Auntor <auntorknocks@gmail.com>
2021-06-22 11:39:59 +0600

    js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 //     const searchText = document.getElementById("search-field").value;
 //     const url = `https://api.lyrics.ovh/suggest/${searchText}`
 //     fetch(url)
-
+//     .then(res => res.json())
+//     .then(data => console.log(data))
 // }
